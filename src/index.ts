093f99e4eadccbe6a1fa74269e013f16f23f9b20
Benjamin Green <bgreen@amplience.com>
2019-11-12 14:19:40 +0000

    export required classes

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { ProductService } from './service/product-service';
+export { ProductResult } from './model/product-result';
+export { Product } from './model/product';
+export { PriceContext } from './model/price-context';
+export { Pagination } from './model/pagination';
+export { ImageContext } from './model/image-context';
